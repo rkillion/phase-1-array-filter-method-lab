d5e3db0e385ac98b4948e22fc8ff15727a3ad161
robertfkillion@gmail.com <robertfkillion@gmail.com>
2021-07-20 11:39:19 -0400

    commit tree c99690ddb063085807f76593d0a0f20f0e923a7c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array,string) {
+    return array.filter(element => element.toLowerCase()===string.toLowerCase());
+}
+
+function fuzzyMatch(array,string) {
+    return array.filter(element => element.substring(0,string.length) === string);
+}
+
+function matchName(array,string) {
+    return array.filter(element => element.name === string);
+}
